6a02f86fecbf61ccbe849c10817ef9a67cda3a98
kevin urbano <kevurbmac@gmail.com>
2017-08-24 18:32:16 +0200

    Carpetas repository y service creados y clases creadas

--- Proyecto_Peliculas/Modelos/Entradas.cs
+++ Proyecto_Peliculas/Modelos/Entradas.cs
@@ -7,6 +7,21 @@ namespace Proyecto_Peliculas.Modelos
 {
     public class Entradas
     {
+        public int id { get; set; }
+        public string salon { get; set; }
+        public int fila { get; set; }
+        public int asiento { get; set; }
+
+        public Entradas()
+        {
+        }
+             
+        public Entradas(string _salon, int _fila, int _asiento)
+        {
+            this.salon = _salon;
+            this.fila = _fila;
+            this.asiento = _asiento;
+        } 
 
     }
 }
