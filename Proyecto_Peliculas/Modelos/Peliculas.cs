cd3fd38a291941254467a0a8c78fad0258eb8a2f
kevin urbano <kevurbmac@gmail.com>
2017-08-24 18:19:45 +0200

    Agregando carpeta de modelos y carpeta de scripts

---
+++ Proyecto_Peliculas/Modelos/Peliculas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto_Peliculas.Modelos
+{
+    public class Peliculas
+    {
+        int id { get; set; }
+        string titulo { get; set; }
+        int duracion { get; set; }
+        string pais { get; set; }
+        string genero { get; set; }
+
+        public Peliculas()
+        {
+        }
+
+        public Peliculas(string _titulo, int _duracion, string _pais, string _genero)
+        {
+            this.titulo = _titulo;
+            this.duracion = _duracion;
+            this.pais = _pais;
+            this.genero = _genero;
+        }
+    }
+}
